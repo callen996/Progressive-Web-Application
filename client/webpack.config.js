c0976f2b1988b89d481ddf547e2be6bee09e3179
Conor Alen <145404862+callen996@users.noreply.github.com>
2024-02-26 22:57:05 -0500

    fixed error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i, 
-          use: ["style-loader", "css.loader"],
+          use: ["style-loader", "css-loader"],
         }
       ],
     },
